a390ab73989003dca1eee8b63e14f80eb01f6ebb
John Warford <johnwarford@Johns-MacBook-Pro.local>
2019-11-25 22:52:25 -0800

    changed broker url

--- verifyPacts.js
+++ verifyPacts.js
@@ -3,7 +3,7 @@ const pact = require('@pact-foundation/pact-node')
 //require('./testProductsService')
 const opts = {
 providerBaseUrl: 'https://jsonplaceholder.typicode.com', // where your service will be running during the test, either staging or localhost on CIs
-pactUrls: ['http://localhost/pacts/provider/GeoApi/consumer/GeoWcf/latest'], // the pacts to test against
+pactUrls: ['http://138.197.140.165/pacts/provider/p1/consumer/c1/latest'], // the pacts to test against
 publishVerificationResult: true,
 providerVersion: '1.0.0'
 }
